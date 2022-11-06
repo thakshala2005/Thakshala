8a87f4a3a4ca57413271792b0545d2ee95c93ef6
thakshala2005 <110020285+thakshala2005@users.noreply.github.com>
2022-11-06 10:36:17 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -29,8 +29,8 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owner v card
-global.vcardowner = ['+380944814219'] // owner number
-global.ownername = "😎𝙆.𝙋𝙧𝙖𝙗𝙝𝙖𝙨𝙝𝙖❯❯" // owner name
+global.vcardowner = ['+94772456135'] // owner number
+global.ownername = "𝗧𝗵𝗮𝗸𝘀𝗵𝗮𝗹𝗮 𝗠𝗶𝗵𝗶𝗿𝗮𝗻𝗴𝗮 😎" // owner name
 global.ytname = "" // yt chanel name
 global.socialm = "GitHub: prabhasha2006" // github or insta name
 global.location = "India, Mizoram, Aizawl" // location
@@ -47,16 +47,16 @@ global.upsongvoice = "false" // true/false \\ bot can upload song as voice note 
 global.welcom_msg = "true" // Welcome / Goodbye   [true/false]
 
 
-global.owner = ['+380944814219']
-global.ownertag = '+380944814219' //your tag number
-global.botname = '𝙍𝙀𝘿╺ 𝘿𝙍𝘼𝙂𝙊𝙉 - 𝘉𝘖𝘛' //ur bot name
+global.owner = ['+94772456135']
+global.ownertag = '+94772456135' //your tag number
+global.botname = '𝗨𝗻𝘀𝘁𝗼𝗽𝗽𝗮𝗯𝗹𝗲 𝗕𝗢𝗧 😎' //ur bot name
 global.packname = "Bot Sticker" // sticker packname
-global.author = "𝙍𝙀𝘿╺ 𝘿𝙍𝘼𝙂𝙊𝙉" // sticker author
+global.author = "𝗨𝗻𝘀𝘁𝗼𝗽𝗽𝗮𝗯𝗹𝗲 𝗕𝗢𝗧 😎" // sticker author
 global.dragonemoji = '🧩' // Menu emoji
 global.footer = '[ ◉ ʀᴇᴅ-ᴅʀᴀɢᴏɴ-ᴍᴅ ]' //
 
 //database
-global.premium = ['+380944814219'] //ur premium numbers
+global.premium = ['+94772456135'] //ur premium numbers
 
 //other
 global.sessionName = 'session'
